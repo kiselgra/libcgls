5254535e4c61a4257ea670e94619d1b16d102dcf
kai <kai@deathmetal>
2012-08-25 22:51:05 +0200

    testfile

---
+++ dp.scm
@@ -0,0 +1,131 @@
+(format #t "Entering ~a.~%" (current-filename))
+
+;;; modules
+(use-modules (ice-9 receive))
+
+;;; actual code
+
+(define x-res 1)
+(define y-res 1)
+
+(receive (x y w h) (viewport)
+  (format #t "~a x ~a~%" w h)
+  (set! x-res w)
+  (set! y-res h))
+
+;(let ((cam (make-perspective-camera "cam" (list 0 0 5) (list 0 0 -1) (list 0 1 0) 35 (/ x-res y-res) 1 1000)))
+;  (use-camera cam))
+
+(load "default.shader")
+
+(let ((home (getenv "HOME")))
+  (append-image-path (string-append home "/render-data/images"))
+  (append-image-path (string-append home "/render-data/images/wikimedia"))
+  (append-image-path (string-append home "/render-data/images/sponza")))
+
+(defmacro cmdline (x)
+  `(query-cmdline ',x))
+
+(define the-scene (make-scene "default"))
+
+(define (custom-uniform-handler de uniform location)
+  (cond ((string=? uniform "light_dir") (gl:uniform3f location 0 -1 -0.2))
+        ((string=? uniform "light_col") (gl:uniform3f location 1 .9 .9))
+		((string=? uniform "hemi_dir") 
+		   (let ((h (cmdline hemi-dir))) 
+		 	 (gl:uniform3f location (car h) (cadr h) (caddr h))))
+		(else #f))
+	)
+
+(define drawelements '())
+
+(define (testcall name mesh material)
+  (let* ((shader (if (cmdline hemi)
+				     (if (material-has-textures? material)
+				         (find-shader "diffuse-hemi+tex")
+				         (find-shader "diffuse-hemi"))
+					 (if (material-has-textures? material)
+					     (find-shader "diffuse-dl+tex")
+						 (find-shader "diffuse-dl"))))
+		 (de (make-drawelement name mesh shader material)))
+    (add-drawelement-to-scene the-scene de)
+	(prepend-uniform-handler de 'default-matrix-uniform-handler)
+	(prepend-uniform-handler de 'default-material-uniform-handler)
+	(prepend-uniform-handler de custom-uniform-handler)
+    (set! drawelements (cons de drawelements))
+	))
+
+(let ((fallback (make-material "fallback" (list 1 0 0 1) (list 1 0 0 1) (list 0 0 0 1))))
+  (receive (min max) (load-objfile-and-create-objects-with-separate-vbos (cmdline model) (cmdline model) testcall fallback)
+    (let* ((near 1)
+		   (far 1000)
+		   (diam (vec-sub max min))
+		   (diam/2 (vec-div-by-scalar diam 2))
+		   (center (vec-add min diam/2))
+		   (distance (vec-length diam))
+		   (pos (vec-add center (make-vec 0 0 distance))))
+	  (while (> near (/ distance 100))
+	    (set! near (/ near 10)))
+	  (while (< far (* distance 2))
+	    (set! far (* far 2)))
+	  (let ((cam (make-perspective-camera "cam" pos (list 0 0 -1) (list 0 1 0) 35 (/ x-res y-res) near far)))
+        (use-camera cam))
+      (set-move-factor! (/ distance 20)))))
+
+
+
+
+ (let ((diffuse-tex (make-texture-without-file "deferred-diff" 'tex-2d w h 'rgba 'rgba-8 'unsigned-byte))
+       (normal-tex (make-texture-without-file "deferred-norm" 'tex-2d w h 'rgba 'rgba-32f 'float))
+       (wpos-tex (make-texture-without-file "deferred-wpos" 'tex-2d w h 'rgba 'rgba-32f 'float))
+       (depth-tex (make-texture-without-file "deferred-depth" 'tex-2d w h 'depth-component 'depth-component-32f 'float))
+       (fbo (make-framebuffer "deferred-fbo" w h)))
+   (let ((texs (list diffuse-tex normal-tex depth-tex))
+         (tex-names (list "diffuse" "normals" "depth")))
+     (apply-to-texs (lambda (t i n) (tex-params! t #f 'nearest 'nearest 'repeat 'repeat)) texs tex-names)
+     (bind-framebuffer fbo)
+     (bind-texture diffuse-tex 0)
+     (attach-texture-as-colorbuffer fbo "diffuse" diffuse-tex)
+     (bind-texture diffuse-tex 1)
+     (attach-texture-as-colorbuffer fbo "normals" normal-tex)
+     (bind-texture diffuse-tex 2)
+     (attach-texture-as-colorbuffer fbo "wpos" wpos-tex)
+     (bind-texture depth-tex 3)
+     (attach-texture-as-depthbuffer fbo "depth" depth-tex)
+     (check-framebuffer-setup fbo)
+     (unbind-framebuffer fbo)
+     (apply-to-texs (lambda (t i n) (unbind-texture t)) texs tex-names)))
+; screenshot fbo
+(let ((color (make-texture-without-file "render-color" 'tex2d w h 'rgba 'rgba-8 'unsigned-byte))
+      (fbo (make-framebuffer "render-fbo" w h)))
+  (tex-params! color #f 'nearest 'nearest 'repeat 'repeat)
+  (bind-framebuffer fbo)
+  (bind-texture color 0)
+  (attach-texture-as-colorbuffer fbo "color" color)
+  (attach-depthbuffer fbo)
+  (check-framebuffer-setup fbo)
+  (unbind-framebuffer fbo)
+  (unbind-texture color)))
+
+
+(define command-queue '())
+(defmacro enqueue (cmd)
+  `(begin
+      (format #t "cmd: .~a.~%" ',cmd)
+      (set! command-queue (cons ',cmd command-queue))))
+(define (apply-commands)
+  (for-each primitive-eval
+            (reverse command-queue))
+  (set! command-queue '()))
+
+(define (display)
+  (gl:clear-color .1 .3 .6 1)
+  (apply-commands)
+  (gl:clear (logior gl#color-buffer-bit gl#depth-buffer-bit))
+  (for-each render-drawelement
+            drawelements)
+  (glut:swap-buffers))
+
+(register-display-function display)
+
+(format #t "Leaving ~a.~%" (current-filename))

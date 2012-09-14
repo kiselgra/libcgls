5ae9b347c6df5527228987801780f179ab2baf8c
kai <kai@istar>
2012-09-15 00:30:52 +0200

    some separation of std:gl:scheme and actual scene.

--- depth-array-oit.scm
+++ depth-array-oit.scm
@@ -1,41 +1,7 @@
 (format #t "Entering ~a.~%" (current-filename))
+(include "std.scm")
 
-;;; modules
-(use-modules (ice-9 receive))
-(use-modules (rnrs bytevectors))
-
-;; nicer cl-style keyword syntax 
-(read-set! keywords 'prefix)
-
-;;; code to definitely move someplace else
-
-(define (mref bv x y)
-  (bytevector-ieee-single-native-ref bv (* 4 (+ (* x 4) y))))
-
-(define (mset! bv x y to)
-  (bytevector-ieee-single-native-set! bv (* 4 (+ (* x 4) y)) to))
-
-(define (print-matrix bv)
-  (if (not (bytevector? bv))
-      (error "not a matrix ~a~%" bv)
-      (do ((y 0 (+ y 1)))
-          ((>= y 4))
-        (do ((x 0 (+ x 1)))
-            ((>= x 4))
-          (display (mref bv x y)) ;bytevector-ieee-single-native-ref bv (* 4 (+ (* x 4) y))))
-          (display "\t"))
-        (newline))))
-
-;;; actual code
-
-(define x-res 1)
-(define y-res 1)
 (define array-layers 20)
-
-(receive (x y w h) (viewport)
-  (format #t "~a x ~a~%" w h)
-  (set! x-res w)
-  (set! y-res h))
 
 (load "default.shader")
 
@@ -209,30 +175,6 @@
   (prepend-uniform-handler de 'default-material-uniform-handler))
 
 
-;; on the fly eval of gl code
-
-(define command-queue '())
-(defmacro enqueue (cmd)
-  `(begin
-      (format #t "cmd: .~a.~%" ',cmd)
-      (set! command-queue (cons ',cmd command-queue))))
-(define (apply-commands)
-  (for-each primitive-eval
-            (reverse command-queue))
-  (set! command-queue '()))
-
-;; convenience
-
-(defmacro disable-color-output body
-  `(begin (gl:color-mask gl#false gl#false gl#false gl#false)
-          ,@body
-          (gl:color-mask gl#true gl#true gl#true gl#true)))
-
-(defmacro disable-depth-output body
-  `(begin (gl:depth-mask gl#false)
-          ,@body
-          (gl:depth-mask gl#true)))
-
 (define gl!!read-write #x088ba)
 
 ;; the display routine registered with glut
@@ -325,7 +267,6 @@
 ;; initial gl setup
 ;;
 (register-display-function display/glut)
-(gl:enable gl#depth-test)
 
 ;; done
 ;;

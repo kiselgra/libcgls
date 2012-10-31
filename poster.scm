cd1871b85dd17afa2d935d12263a9f4a5ddddace
kai <kai@deathmetal>
2012-10-31 18:16:14 +0100

    poster base.

--- poster.scm
+++ poster.scm
@@ -3,7 +3,7 @@
 
 (define memory-factor 10)
 
-(load "tsm.shader")
+(load "poster.shader")
 
 (let ((home (getenv "HOME")))
   (append-image-path (string-append home "/render-data/images"))
@@ -21,43 +21,6 @@
 (define shadow-cam (make-perspective-camera "shadowcam" spot-pos spot-dir (make-vec  0.29 0.83 0.48) 25 1 1 5000));16384))
 
 (define gl#compare-r-to-texture gl#compare-ref-to-texture)
-
-(define (make-gbuffer w h)
-  (letrec ((diffuse-tex  (make-texture-without-file "gbuffer/diff" gl#texture-2d w h gl#rgba gl#rgba8 gl#unsigned-byte))
-	   (specular-tex (make-texture-without-file "gbuffer/spec" gl#texture-2d w h gl#rgba gl#rgba8 gl#unsigned-byte))
-	   (normal-tex   (make-texture-without-file "gbuffer/norm" gl#texture-2d w h gl#rgba gl#rgba16f gl#unsigned-short))
-	   (position-tex (make-texture-without-file "gbuffer/wpos" gl#texture-2d w h gl#rgba gl#rgba32f gl#float))
-	   (depth-tex    (make-texture-without-file "gbuffer/depth" gl#texture-2d w h gl#depth-component gl#depth-component32f gl#float))
-	   (fbo          (make-framebuffer "gbuffer" w h))
-	   (link         (lambda (fun tex pos name)
-			   (bind-texture tex pos)
-			   (fun fbo name tex))))
-    (bind-framebuffer fbo)
-    (bind-texture diffuse-tex 0)
-    (link attach-texture-as-colorbuffer diffuse-tex  0 "diffuse")
-    (link attach-texture-as-colorbuffer specular-tex 1 "specular")
-    (link attach-texture-as-colorbuffer normal-tex   2 "normal")
-    (link attach-texture-as-colorbuffer position-tex 3 "position")
-    (link attach-texture-as-depthbuffer depth-tex    4 "depth" )
-    (check-framebuffer-setup fbo)
-    (unbind-framebuffer fbo)
-    (for-each unbind-texture (list diffuse-tex specular-tex normal-tex position-tex depth-tex))
-    ; return our handler
-    (lambda (msg . x)
-      (case (msg)
-	((bind) (bind-framebuffer fbo))
-	((unbind) (unbind-framebuffer fbo))
-	((tex) (case (car x)
-		 ((diffuse)  diffuse-tex)
-		 ((specular) specular-tex)
-		 ((normal)   normal-tex)
-		 ((position) position-tex)
-		 ((depth)    depth-tex)
-		 (else       #f)))
-	((size) (values w h))
-	(else #f)))))
-
-(define gbuffer (make-gbuffer x-res y-res))
 
 ;; scene handling
 (define (light-uniform-handler de uniform location)
@@ -79,66 +42,15 @@
 	((string=? "tex4" uniform) (gl:uniform1i location 4) #t)
 	(else #f)))
 
-(define (make-mm-shadow-buffers name w h storage-factor)
-  (define (setup-shadow-fbo)
-    (let ((depth (make-texture-without-file "shadow-depth" gl#texture-2d w h gl#depth-component gl#depth-component32f gl#float))
-	  (color (make-texture-without-file "shadow-color" gl#texture-2d w h gl#rgba gl#rgba8 gl#unsigned-byte)) ; for debug visualization
-	  (fbo (make-framebuffer "shadow" w h)))
-      (bind-framebuffer fbo)
-      (bind-texture depth 0)
-      (attach-texture-as-colorbuffer fbo (texture-name color) color)
-      (attach-texture-as-depthbuffer fbo (texture-name depth) depth)
-      (check-framebuffer-setup fbo)
-      (unbind-framebuffer fbo)))  
-  (setup-shadow-fbo)
-
-  (let ((frag-head-name (string-append name "_" "head_buffer"))
-	(frag-tail-name (string-append name "_" "tail_buffer"))
-	(frag-depths-name (string-append name "_" "frag_depths"))
-	(wh-name (string-append name "_" "buffer_size")))
-    ;; buffers for depth array management
-    (let ((frag-head (make-texture-without-file frag-head-name gl#texture-2d w h gl#red gl#r32f gl#float))
-	  (frag-tail (make-texture-without-file frag-tail-name gl#texture-2d w (* h storage-factor) gl#red gl#r32f gl#float))
-	  (frag-depths (make-texture-without-file frag-depths-name gl#texture-2d w (* h storage-factor) gl#red gl#r32f gl#float)))
-      ;; buffer ids
-      (let ((frag-head-id 0)
-	    (frag-tail-id 1)
-	    (frag-depths-id 3))
-	(let ((handler (lambda (de u loc)
-			 (cond ((string=? u frag-head-name) (gl:uniform1i loc frag-head-id))
-			       ((string=? u frag-tail-name) (gl:uniform1i loc frag-tail-id))
-			       ((string=? u frag-depths-name) (gl:uniform1i loc frag-depths-id))
-			       ((string=? u wh-name) (gl:uniform2i loc w h))
-			       (else #f))))
-	      (short-handler (lambda (de u loc)
-			       (cond ((string=? u "head_buffer") (gl:uniform1i loc frag-head-id))
-				     ((string=? u "tail_buffer") (gl:uniform1i loc frag-tail-id))
-				     ((string=? u "frag_depths") (gl:uniform1i loc frag-depths-id))
-				     ((string=? u "wh") (gl:uniform2i loc w h))
-				     (else #f))))
-	      (bind (lambda (args)
-		      (for-each (lambda (arg) (case arg
-						((head) (bind-texture-as-image frag-head frag-head-id 0 gl!!read-write gl#r32i))
-						((tail) (bind-texture-as-image frag-tail frag-tail-id 0 gl!!read-write gl#r32i))
-						((depths) (bind-texture-as-image frag-depths frag-depths-id 0 gl!!read-write gl#r32f))
-						(else (throw 'unknown-mm-buffer arg))))
-				args)))
-	      (unbind (lambda (args)
-			(for-each (lambda (arg) (case arg
-						  ((head) (unbind-texture-as-image frag-head frag-head-id))
-						  ((tail) (unbind-texture-as-image frag-tail frag-tail-id))
-						  ((depths) (unbind-texture-as-image frag-depths frag-depths-id))
-						  (else (throw 'unknown-mmsm-buffer arg))))
-				  args))))
-	  (lambda (message . args)
-	    (case message
-	      ((handle) handler)
-	      ((handle-shorthand) short-handler)
-	      ((bind) (bind args))
-	      ((unbind) (unbind args))
-	      ((size) (values w h))
-	      (else (throw 'unknown-mm-shadow-message message)))))))))
-
+(define (setup-shadow-fbo w h)
+  (let ((depth (make-texture-without-file "shadow-depth" gl#texture-2d w h gl#depth-component gl#depth-component32f gl#float))
+	(fbo (make-framebuffer "shadow" w h)))
+    (bind-framebuffer fbo)
+    (bind-texture depth 0)
+    (attach-texture-as-depthbuffer fbo (texture-name depth) depth)
+    (check-framebuffer-setup fbo)
+    (unbind-framebuffer fbo)))  
+(setup-shadow-fbo 1024 1024)
 
 (define (shaodw-uniform-handler de uniform location)
   (cond ((string=? uniform "shadow_view") (uniform-camera-view-matrix location shadow-cam))
@@ -146,23 +58,8 @@
         ((string=? uniform "shadow_map") (gl:uniform1i location 7))
         (else #f)))
 	
-(define mmsm (make-mm-shadow-buffers "shadow" 1024 1024 10))
-;(define mmsm (make-mm-shadow-buffers "shadow" x-res y-res 10))
-(define (mmsm-handler de u l)
-  ((mmsm 'handle) de u l))
-
 (define mouse-x 0)
 (define mouse-y 0)
-(define level 1)
-(define (selection-uniform-handler de u l)
-  (receive (w h) (mmsm 'size)
-    (cond ((string=? u "xywh") (gl:uniform4i l (floor (* w (/ mouse-x x-res))) (floor (- h (* h (/ mouse-y y-res)))) (1- (expt 2 level)) (1- (expt 2 level))) #t)
-	  (else #f))))
-
-;; this one is for debugging, only.
-(define atomic-counter (make-atomic-buffer "test" 1 1))
-(define copy-of-atomic-buffer #f)
-
 
 (define verbose-passes #f)
 (defmacro make-pass (name drawelements classification . body)
@@ -184,7 +81,6 @@
 			((run) (check-for-gl-errors (string-append "before " ,name))
 			 (if verbose-passes (format #t "--> ~a~%" name))
 			 (gl:finish 0)
-			 (memory-barrier!!)
 			 (let ((start (glut:time-stamp)))
 			   (body)
 			   (let* ((stop (glut:time-stamp))
@@ -192,7 +88,6 @@
 			     (vector-set! timer tidx diff)
 			     (set! tidx (modulo (1+ tidx) 20))))
 			 (gl:finish 0)
-			 (memory-barrier!!)
 			 (check-for-gl-errors (string-append "after " ,name)))
 			((reset-drawelements!) (set! drawelements (car args)) (classify))
 			((time) (/ (reduce + 0 (vector->list timer)) 20))
@@ -216,8 +111,7 @@
  
 (load "sceneloader.scm")
 (register-scene-uniform-handlers light-uniform-handler
-				 shaodw-uniform-handler
-				 mmsm-handler)
+				 shaodw-uniform-handler)
 (load-scene)
 (setup-opaque-rendering)
 
@@ -228,31 +122,7 @@
        (tqsh (find-shader "texquad-with-depth"))
        (de (make-drawelement "texquad" tqme tqsh tqma)))
   (prepend-uniform-handler de 'default-material-uniform-handler)
-  (prepend-uniform-handler de mmsm-handler)
-  (prepend-uniform-handler de selection-uniform-handler)
   (prepend-uniform-handler de lax-tex-uniform-handler))
-
-(let* ((tqma (make-material "texquad/ca" (make-vec 0 0 0 1) (make-vec 0 1 0 1) (make-vec 0 0 0 1)))
-       (tqme (make-quad "texquad/clear-array"))
-       (tqsh (find-shader "texquad/clear-array"))
-       (de (make-drawelement "texquad/ca" tqme tqsh tqma)))
-  (prepend-uniform-handler de mmsm-handler)
-  (prepend-uniform-handler de 'default-material-uniform-handler))
- 
-(let* ((tqma (make-material "texquad/cc" (make-vec 0 0 0 1) (make-vec 0 1 0 1) (make-vec 0 0 0 1)))
-       (tqme (make-quad "texquad/clear-color"))
-       (tqsh (find-shader "texquad/clear-color"))
-       (de (make-drawelement "texquad/cc" tqme tqsh tqma)))
-  (prepend-uniform-handler de mmsm-handler)
-  (prepend-uniform-handler de 'default-material-uniform-handler))
-
-(let* ((f-mat (make-material "frumat" (make-vec 1 1 0 1) (make-vec 1 1 0 1) (make-vec 0 0 0 1)))
-       (f-mesh (make-simple-wire-frustum "frufru" spot-pos spot-dir (make-vec  0.29 0.83 0.48) 25 1 10 5000))
-       (f-shader (find-shader "solid-line"))
-       (de (make-drawelement "frustum" f-mesh f-shader f-mat)))
-  (prepend-uniform-handler de 'default-material-uniform-handler)
-  (prepend-uniform-handler de 'default-matrix-uniform-handler)
-  )
 
 (defmacro with-viewport (vp . body)
   (let-optional vp (ox oy dx dy)
@@ -261,10 +131,6 @@
        (let ((res (begin ,@body)))
          (gl:viewport oldox oldoy olddx olddy)
          res))))
-
-(define gl!!read-only #x88B8)
-(define gl!!write-only #x88B9)
-(define gl!!read-write #x088ba)
 
 (define all-passes '())
 
@@ -282,48 +148,7 @@
 		 (disable-color-output
 		   (render-drawelements))
 		 (unbind-framebuffer fbo)
-		 ;(bind-texture sm 0)
-		 ;(save-texture/png sm "shadow.png")
-		 ;(unbind-texture sm)
 		 (gl:disable gl#polygon-offset-fill)))))
-
-(define clear-shadow-arrays-pass
-  (let ((clear-quad-1 (find-drawelement "texquad/clear-array"))
-	(clear-quad-2 (find-drawelement "texquad/clear-color")))
-    (make-pass "clear shadow arrays" '()
-	       (lambda (de) #f)
-	       (begin
-		 (mmsm 'bind 'head 'tail)
-		 (render-drawelement clear-quad-1)
-		 (mmsm 'unbind 'head 'tail)
-		 (mmsm 'bind 'depths)
-		 (render-drawelement clear-quad-1)
-		 (mmsm 'unbind 'depths)))))
-
-(define shadow-frag-collector-pass
-  (let ((shader (find-shader "shadow-frag-collect")))
-    (receive (w h) (mmsm 'size)
-      (make-pass "collect shadow fragments" scene-drawelements
-		 (lambda (de)
-		   shader)
-		 (with-viewport (0 0 w h)
-	           (reset-atomic-buffer atomic-counter 0)
-		   (bind-atomic-buffer atomic-counter 0)
-		   (gl:enable gl#polygon-offset-fill)
-		   (gl:polygon-offset 1 1)
-		   (disable-color-output
-		    (disable-depth-output
-		     (mmsm 'bind 'depths 'head 'tail)
-		     (render-drawelements)
-		     (mmsm 'unbind 'depths 'head 'tail)))
-		   (gl:disable gl#polygon-offset-fill)
-		   (gl:finish 0)
-		   (memory-barrier!!)
-		   (unbind-atomic-buffer atomic-counter 0)
-		   (gl:finish 0) ;; bug in wrapper/gen -> glFinish(void);
-		   (set! copy-of-atomic-buffer (read-atomic-buffer atomic-counter))
-		   (format #t "frag-counter: ~a~%" (bytevector-s32-native-ref copy-of-atomic-buffer 0))
-		   )))))
 
 (define base-image-pass
   (let ((fbo (find-framebuffer "colorbuffer"))
@@ -337,21 +162,7 @@
 		 (gl:clear (logior gl#color-buffer-bit gl#depth-buffer-bit))
 		 (bind-texture shadow-depth 7)
 		 (render-drawelements)
-                 (render-drawelement (find-drawelement "frufru"))
 		 (unbind-texture shadow-depth)
-		 (unbind-framebuffer fbo)
-		 ))))
-
-(define shadowcam-image-pass
-  (let ((fbo (find-framebuffer "shadow")))
-    (make-pass "shadowcam image" scene-drawelements
-	       (lambda (de)
-		 (find-shader (string-append (shader-name (drawelement-shader de)) "/noshadow")))
-	       (begin
-		 (bind-framebuffer fbo)
-		 (gl:clear-color .1 .3 .6 1)
-		 (gl:clear (logior gl#color-buffer-bit gl#depth-buffer-bit))
-		 (render-drawelements)
 		 (unbind-framebuffer fbo)
 		 ))))
 
@@ -370,57 +181,6 @@
 		 (unbind-texture depthtex)
 		 (unbind-texture coltex)))))
 
-(define show-shadowcam-image-pass
-  (let ((coltex (find-texture "shadow-color"))
-	(depthtex (find-texture "shadow-depth"))
-	(texquad (find-drawelement "texquad"))
-	(counter-vis (find-shader "frag-count-vis")))
-    (make-pass "show shadowcam image" '()
-	       (lambda (de) #f)
-	       (begin
-		 (bind-texture coltex 0)
-		 (bind-texture depthtex 1)
-		 (gl:disable gl#depth-test)
-		 (if (eq? render-mode 'shadow-frag-len)
-		     (begin
-		       (mmsm 'bind 'depths 'head 'tail)
-		       (render-drawelement-with-shader texquad counter-vis)
-		       (mmsm 'unbind 'depths 'head 'tail))
-		     (render-drawelement texquad))
-		 (gl:enable gl#depth-test)
-		 (unbind-texture depthtex)
-		 (unbind-texture coltex)))))
-
-(define show-shadowcam-sort
-  (let ((texquad (find-drawelement "texquad"))
-	(sort-f (find-shader "sort-shadow-frags")))
-    (receive (w h) (mmsm 'size)
-      (make-pass "sort fragments (and output some dummy color)" '()
-		 (lambda (de) #f)
-		 (begin
-		   (gl:disable gl#depth-test)
-		   (with-viewport (0 0 w h)
-		     (mmsm 'bind 'depths 'head 'tail)
-		     (render-drawelement-with-shader texquad sort-f)
-		     (mmsm 'unbind 'depths 'head 'tail))
-		   (gl:enable gl#depth-test))))))
-
-(define show-shadowcam-sort-bla2
-  (let ((coltex (find-texture "shadow-color"))
-	(depthtex (find-texture "shadow-depth"))
-	(texquad (find-drawelement "texquad"))
-	(sort-c (find-shader "check-shadow-sort")))
-    (receive (w h) (mmsm 'size)
-      (make-pass "check fragment ordering" '()
-		 (lambda (de) #f)
-		 (begin
-		   (gl:disable gl#depth-test)
-		   (with-viewport (0 0 w h)
-		     (mmsm 'bind 'depths 'head 'tail)
-		     (render-drawelement-with-shader texquad sort-c)
-		     (mmsm 'unbind 'depths 'head 'tail))
-		   (gl:enable gl#depth-test))))))
-	
 (define whole-frame-time 0)
 (define number-of-frames 0)
 (define (print-pass-timings)
@@ -431,25 +191,6 @@
   (set! whole-frame-time 0)
   (set! number-of-frames 0))
 
-
-;; the display routine registered with glut
-(set-move-factor! (/ (move-factor) 2))
-
-(define render-mode 'default)
-
-(defmacro start-timer ()
-  `(set! t-start (glut:time-stamp)))
-(defmacro with-timer (end . body)
-  `(begin
-      (gl:finish 0)
-      (memory-barrier!!)
-      (let ((timer-start (glut:time-stamp)))
-        ,@body
-        (gl:finish 0)
-        (memory-barrier!!)
-        (set! ,end (+ ,end (- (glut:time-stamp) timer-start))))))
-
-(define fps 'not-ready)
 (define print-timings #t)
 
 (let* (; the opaque render target
@@ -468,115 +209,45 @@
             (set! print-timer (glut:time-stamp)))))
        ; the actual display function
        (display/glut 
-         (lambda ()
-           (apply-commands)
-           (print-timings)
+	(lambda ()
+	  (apply-commands)
+	  (print-timings)
 
-           (when spot-follow-cam
-             (set! spot-pos (vec-add (cam-pos (current-camera)) (make-vec 4 0 2)))
-             (set! spot-dir (cam-dir (current-camera))))
+	  (when spot-follow-cam
+	    (set! spot-pos (vec-add (cam-pos (current-camera)) (make-vec 4 0 2)))
+	    (set! spot-dir (cam-dir (current-camera))))
 
-	   (with-viewport (0 0 10 10)
-			  (let ((x 1)) x)
-			  )
-
-	   (set! t-frame 0)
-	   (with-timer
-	     t-frame
-	     (check-for-gl-errors "right at the beginning")
-
-;	     (begin   ;; shadow stuff
-;	       (use-camera (find-camera "shadowcam"))
-;	       (shadow-map-pass 'run)
-;	       (clear-shadow-arrays-pass 'run)
-;	       (transparent-shadow-list-pass 'run)
-;	       (use-camera (find-camera "cam")))
-	     (case render-mode
-	       ((default)
-		(begin
-		  (use-camera (find-camera "shadowcam"))
-		  (shadow-map-pass 'run)
-		  (clear-shadow-arrays-pass 'run)
-		  (shadow-frag-collector-pass 'run)
-		  (use-camera (find-camera "scene-cam"))
-		  (show-shadowcam-sort 'run)
-		  
-		  ;; generate the base image.
-		  (base-image-pass 'run)
-		  (show-base-image-pass 'run)
-		  ))
-
-	       ((shadowmap shadow-frag-len)
-		(begin
-		  (use-camera (find-camera "shadowcam"))
-		  (shadowcam-image-pass 'run)
-		  (show-shadowcam-image-pass 'run)))
-
-	       ((sort-vis sort-vis2)
-		(begin
-		  (use-camera (find-camera "shadowcam"))
-		  (shadow-map-pass 'run)
-		  (clear-shadow-arrays-pass 'run)
-		  (shadow-frag-collector-pass 'run)
-		  (use-camera (find-camera "scene-cam"))
-		  (show-shadowcam-sort 'run)
-		  (if (eq? render-mode 'sort-vis2)
-		      (show-shadowcam-sort-bla2 'run))
-
-		  (let* ((head-tex (find-texture "shadow_head_buffer"))
-			 (tail-tex (find-texture "shadow_tail_buffer"))
-			 (depth-tex (find-texture "shadow_frag_depths"))
-			 (head (download-texture1f head-tex))
-			 (tail (download-texture1f tail-tex))
-			 (depth (download-texture1f depth-tex))
-			 (int (lambda (bv i) (bytevector-s32-native-ref bv (* 4 i))))
-			 (float (lambda (bv i) (bytevector-ieee-single-native-ref bv (* 4 i))))
-			 (for-each-pixel (lambda (proc)
-					   (receive (w h) (mmsm 'size)
-					     (do ((y 0 (1+ y))) ((>= y h))
-					       (do ((x 0 (1+ x))) ((>= x w))
-						 (proc w h x y))))))
+	  (with-viewport (0 0 10 10)
+			 (let ((x 1)) x)
 			 )
-		    (for-each-pixel (lambda (w h x y)
-				      (let traverse-tail ((d -1) (link (int head (+ (* y w) x))) (i 0))
-					(if (and (>= link 0) (< i 16))
-					    (let ((next-d (float depth link)))
-					      (if (> d next-d)
-						  (format #t "error at pixel (~a, ~a)@~a, ~7,6f > ~7,6f!~%" x y i d next-d)
-						  (traverse-tail next-d (int tail (+ (remainder link w) (* w (floor (/ link w))))) (1+ i)))))))))
-		  ))
 
+	  (check-for-gl-errors "right at the beginning")
 
-	       )   ; rendermode
+	  (begin
+	    (use-camera (find-camera "shadowcam"))
+	    (shadow-map-pass 'run)
+	    (use-camera (find-camera "scene-cam"))
+	    
+	    ;; generate the base image.
+	    (base-image-pass 'run)
+	    (show-base-image-pass 'run)
+	    )
 
-             (glut:swap-buffers))
-             (set! whole-frame-time (+ whole-frame-time t-frame))
-	     (set! number-of-frames (1+ number-of-frames))
-	     )))
+	  (glut:swap-buffers)
+	  (set! whole-frame-time (+ whole-frame-time t-frame))
+	  (set! number-of-frames (1+ number-of-frames)))))
   (register-display-function display/glut))
 
 
 (define (key ch x y)
-  (let ((ms (lambda () (format #t "switched render-mode to ~a.~%" render-mode))))
-    (case ch
-      ((#\1) (set! render-mode 'default) (ms))
-      ((#\2) (set! render-mode 'shadowmap) (ms))
-      ((#\3) (set! render-mode 'shadow-frag-len) (ms))
-      ((#\4) (set! render-mode 'sort-vis) (ms))
-      ((#\5) (set! render-mode 'sort-vis2) (ms))
-      ((#\+) (set! level (1+ level)))
-      ((#\-) (set! level (1- level)))
-      (else
-        (if (eq? render-mode 'default)
-	    (standard-key-function ch x y))))))
+  (standard-key-function ch x y))
 (register-key-function key)
 
 (define (momo x y)
-  (if (eq? render-mode 'default)
-      (standard-mouse-motion-function x y)
-      (begin
-	(set! mouse-x x)
-	(set! mouse-y y))))
+  (standard-mouse-motion-function x y)
+  (begin
+    (set! mouse-x x)
+    (set! mouse-y y)))
 (register-mouse-motion-function momo)
 
 (define (reload-shaders)
--- scene:sponza+stanford.scm
+++ scene:sponza+stanford.scm
@@ -1,6 +1,7 @@
 (define use-dragon #f)
 
-(setup-scene "/home/kai/render-data/models/sponza.obj")
+(let ((home (getenv "HOME")))
+(setup-scene (string-append home "/render-data/models/sponza.obj"))
 
 (let* ((oldcam (find-camera "scene-cam"))
        (near (cam-near oldcam))
@@ -11,11 +12,11 @@
 
 
   (let ((bunny-mat (make-material "bunnymat" (make-vec 0 0 0 0) (make-vec .8 0 0 .2) (make-vec 0 0 0 1))))
-    (add-model-to-scene "/home/kai/render-data/models/bunny-70k.obj" "bunny70k" bunny-mat))
+    (add-model-to-scene (string-append home "/render-data/models/bunny-70k.obj") "bunny70k" bunny-mat))
   
   (if use-dragon
       (let ((dragon-mat (make-material "dragonmat" (make-vec 0 0 0 0) (make-vec 0 .7 0 .2) (make-vec 0 0 0 1))))
-	(add-model-to-scene "/home/kai/render-data/models/drache.obj" "dragon" dragon-mat)))
+	(add-model-to-scene (string-append home "/home/kai/render-data/models/drache.obj") "dragon" dragon-mat)))
   
   (let ((bunny (find-drawelement "bunny70k/bunny"))
         (trafo (make-rotation-matrix (make-vec 1 0 0) (/ 3.1416 -2))))
@@ -33,4 +34,4 @@
 	(mset! trafo 3 1 -43)
 	(mset! trafo 3 2 250)
 	(set-de-trafo! dragon trafo)))
-
+)

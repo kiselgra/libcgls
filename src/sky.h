7418998bdda0b90691d62135cd60b6a6515fe650
kai <kai@deathmetal>
2013-08-28 21:04:26 +0200

    sky bugfixes.

--- src/sky.h
+++ src/sky.h
@@ -1,7 +1,15 @@
 #ifndef __SKY_H__ 
 #define __SKY_H__ 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 drawelement_ref make_skybox_with_spherical_mapping(const char *dename, const char *filename);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
